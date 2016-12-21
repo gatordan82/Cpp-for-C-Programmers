facff731f982c9955aa6a18e0ca859db17085ecd
gatordan82 <gatordan82@gmail.com>
2016-12-20 23:16:58 -0500

    Added C version of DiceProbability from the Week 1 Lectures.

--- Part A - Week 1/DiceProbability.cpp
+++ Part A - Week 1/DiceProbability.cpp
@@ -20,7 +20,7 @@ int main()
 	int trials;
 	cin >> trials;
 
-	int* outcomes = new int[n_dice * sides + 1]();
+	int* outcomes = new int[n_dice * sides + 1]{};
 
 	for (int j = 0; j < trials; ++j)
 		outcomes[u(e) + u(e)]++;
@@ -30,8 +30,8 @@ int main()
 	for (int j = 2; j < n_dice * sides + 1; ++j)
 	{
 		cout << "j = " << j << " p = "
-			<< static_cast<double>(outcomes[j]) / trials
-			<< endl;
+			 << static_cast<double>(outcomes[j]) / trials
+			 << endl;
 	}
 
 	delete[] outcomes;
