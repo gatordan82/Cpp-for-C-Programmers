75928e8394aeb198ac70905354563ac45d93ef80
Dan <gatordan82@gmail.com>
2017-01-07 16:17:02 -0500

    Added some methods for IndexMinPriorityQueue

--- Part A - HW 2/IndexMinPriorityQueue.h
+++ Part A - HW 2/IndexMinPriorityQueue.h
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <functional>
 #include <utility>
+#include <exception>
 
 template <class Key, class Index=size_t>
 class IndexMinPriorityQueue
@@ -29,8 +30,8 @@ public:
 	void insert(Key& k, Index& i);
 	Key& minKey() const;
 	Index& minIndex() const;
-	pair& removeMin();
-	Key& keyOf(Index& i);
+	Index& removeMin();
+	Key& keyOf(Index& i) const;
 	void changeKey(Key& newPriority, Index& i);
 	void delete(Index& i);
 
@@ -53,5 +54,99 @@ inline IndexMinPriorityQueue<Key, Index>::IndexMinPriorityQueue(size_t size)
 
 template<class Key, class Index>
 inline IndexMinPriorityQueue<Key, Index>::IndexMinPriorityQueue(const IndexMinPriorityQueue<Key, Index>& pq)
+	: _heap{ pq._heap }
+{
+}
+
+template<class Key, class Index>
+inline IndexMinPriorityQueue<Key, Index>::~IndexMinPriorityQueue()
+{
+}
+
+template<class Key, class Index>
+inline IndexMinPriorityQueue<Key, Index>& IndexMinPriorityQueue<Key, Index>::operator=(const IndexMinPriorityQueue<Key, Index>& pq)
+{
+	if (&pq != this)
+	{
+		_heap = pq._heap;
+	}
+
+	return *this;
+}
+
+template<class Key, class Index>
+inline bool IndexMinPriorityQueue<Key, Index>::isEmpty() const
+{
+	return (_heap.size == 0);
+}
+
+template<class Key, class Index>
+inline bool IndexMinPriorityQueue<Key, Index>::containsIndex(Index& i) const
+{
+	const auto& it = std::find_if(_heap.begin(), _heap.end(), 
+		[](const pair& p)
+	{
+		p.second == i;
+	});
+
+	return (it != _heap.end());
+}
+
+template<class Key, class Index>
+inline size_t IndexMinPriorityQueue<Key, Index>::size() const
+{
+	return _heap.size();
+}
+
+template<class Key, class Index>
+inline void IndexMinPriorityQueue<Key, Index>::insert(Key& k, Index& i)
+{
+	_heap.push_back(pair{ k, i });
+	std::push_heap(_heap.begin(), _heap.end());
+}
+
+template<class Key, class Index>
+inline Key& IndexMinPriorityQueue<Key, Index>::minKey() const
+{
+	if (size() == 0)
+		throw std::length_error{ "Priority Queue is empty." };
+
+	return _heap[0].first;
+}
+
+template<class Key, class Index>
+inline Index& IndexMinPriorityQueue<Key, Index>::minIndex() const
+{
+	if (size() == 0)
+		throw std::length_error{ "Priority Queue is empty." };
+
+	return _heap[0].second;
+}
+
+template<class Key, class Index>
+inline typename Index& IndexMinPriorityQueue<Key, Index>::removeMin()
+{
+	if (size() == 0)
+		throw std::length_error{ "Priority Queue is empty." };
+
+	std::pop_heap(_heap.begin(), _heap.end());
+	
+	return _heap.pop_back().second;
+}
+
+template<class Key, class Index>
+inline Key& IndexMinPriorityQueue<Key, Index>::keyOf(Index& i) const
+{
+	const auto& it = std::find_if(_heap.begin(), _heap.end(),
+		[](const pair& p)
+	{
+		p.second == i;
+	});
+
+	return it->first;
+}
+
+template<class Key, class Index>
+inline void IndexMinPriorityQueue<Key, Index>::changeKey(Key& newPriority, Index & i)
 {
 }
