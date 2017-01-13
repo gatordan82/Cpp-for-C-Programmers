31900c659833dbff70198e0fa2816a42c8cb4e6f
gatordan82 <gatordan82@gmail.com>
2017-01-12 19:26:36 -0500

    Started to add some Week 4 code.

---
+++ Part A - Week 4/Point.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <iostream>
+
+class point
+{
+private:
+	double x, y;
+
+public:
+	point() : point(0, 0) {}
+	point(double u) : x(u), y(0.0) {}
+	point(double u, double v) : x(u), y(v) {}
+	operator double() { return sqrt(x * x + y * y); }
+	friend std::ostream& operator<<(std::ostream& out, const point& p)
+	{
+		out << "(" << p.x << ", " << p.y << ")";
+		return out;
+	}
+
+	point operator+(point p)
+	{
+		return point(x + p.x, y + p.y);
+	}
+};
+
