ae23af5cbde73067db885a3d64b2a883ee9f33b6
gatordan82 <gatordan82@gmail.com>
2017-01-12 21:57:44 -0500

    Completed MinPriorityQueue.

--- Part A - HW 3/MinPriorityQueue.h
+++ Part A - HW 3/MinPriorityQueue.h
@@ -2,20 +2,170 @@
 #define _MIN_PRIORITY_QUEUE_H
 
 #include <vector>
+#include <exception>
 
 template <class Key=double>
 class MinPriorityQueue
 {
 private:
 	std::vector<Key> _pq;
-	size_t N;
-
+	size_t _N;
 
 public:
+	// Constructors
 	MinPriorityQueue();
-	MinPriorityQueue(size_t initialCapacity);
+	explicit MinPriorityQueue(size_t initialSize);
+	explicit MinPriorityQueue(std::vector<Key> keys);
+	MinPriorityQueue(MinPriorityQueue<Key>& minPQ);
+
+	~MinPriorityQueue();
+
+	MinPriorityQueue<Key>& operator=(MinPriorityQueue<Key>& minPQ);
+
+	// Priority Queue API
+	bool isEmpty() const;
+	size_t size() const;
+	const Key& min() const;
+	void insert(Key& key);
+	Key removeMin();
 
 
+private:
+	// Helper functions
+	void sink(size_t k);
+	void swim(size_t k);
+	bool greater(size_t i, size_t j) const;
+	void exchange(size_t i, size_t j);
 };
 
 #endif // !_MIN_PRIORITY_QUEUE_H
+
+template<class Key>
+inline MinPriorityQueue<Key>::MinPriorityQueue()
+	: _pq{ std::vector<Key>{} },
+	_N{ 0 }
+{
+}
+
+template<class Key>
+inline MinPriorityQueue<Key>::MinPriorityQueue(size_t initialSize)
+	: _pq{ std::vector<Key>{initialSize + 1, Key{}} },
+	_N{ 0 }
+{
+}
+
+template<class Key>
+inline MinPriorityQueue<Key>::MinPriorityQueue(std::vector<Key> keys)
+	: _pq{ keys },
+	_N{ keys.size()}
+{
+	for (size_t i{ 0 }; i < _N; ++i)
+		_pq[i + 1] = keys[i];
+	for (size_t k{ _N / 2 }; k >= 1; --k)
+		sink(k);
+}
+
+template<class Key>
+inline MinPriorityQueue<Key>::MinPriorityQueue(MinPriorityQueue<Key>& minPQ)
+	: _pq{ minPQ._pq },
+	_N{ minPQ._N }
+{
+}
+
+template<class Key>
+inline MinPriorityQueue<Key>::~MinPriorityQueue()
+{
+}
+
+template<class Key>
+inline MinPriorityQueue<Key>& MinPriorityQueue<Key>::operator=(MinPriorityQueue<Key>& minPQ)
+{
+	if (&minPQ != this)
+	{
+		_pq = minPQ._pq;
+		_N = minPQ._N;
+	}
+
+	return *this;
+}
+
+
+template<class Key>
+inline bool MinPriorityQueue<Key>::isEmpty() const
+{
+	return (_N == 0);
+}
+
+template<class Key>
+inline size_t MinPriorityQueue<Key>::size() const
+{
+	return _N;
+}
+
+template<class Key>
+inline const Key& MinPriorityQueue<Key>::min() const
+{
+	if (isEmpty()) throw std::underflow_error("Priority Queue is empty.");
+
+	return _pq[1];
+}
+
+template<class Key>
+inline void MinPriorityQueue<Key>::insert(Key& key)
+{
+	_pq[++_N] = key;
+	swim(_N);
+}
+
+template<class Key>
+inline Key MinPriorityQueue<Key>::removeMin()
+{
+	if (isEmpty()) throw std::underflow_error("Priority Queue is empty.");
+
+	exchange(1, _N);
+	Key min{ _pq[_N--] };
+	sink(1);
+
+	return min;
+}
+
+template<class Key>
+inline void MinPriorityQueue<Key>::sink(size_t k)
+{
+	while (2 * k <= _N)
+	{
+		size_t j{ 2 * k };
+		if (j < _N && greater(j, j + 1)) j++;
+		if (!greater(k, j)) break;
+		exchange(k, j);
+		k = j;
+	}
+}
+
+template<class Key>
+inline void MinPriorityQueue<Key>::swim(size_t k)
+{
+	while (k > 1 && greater(k / 2, k))
+	{
+		exchange(k, k / 2);
+		k = k / 2;
+	}
+}
+
+template<class Key>
+inline bool MinPriorityQueue<Key>::greater(size_t i, size_t j) const
+{
+	return (_pq[i] > _pq[j]);
+}
+
+template<class Key>
+inline void MinPriorityQueue<Key>::exchange(size_t i, size_t j)
+{
+	Key swap{ _pq[i] };
+	_pq[i] = _pq[j];
+	_pq[j] = swap;
+}
+
+
+
+
--- Part A - HW 3/main.cpp
+++ Part A - HW 3/main.cpp
@@ -1,7 +1,30 @@
 #include "Graph.h"
 #include "WeightedQuickUnionPathCompressionUF.h"
+#include "MinPriorityQueue.h"
+
+using namespace std;
 
 int main()
 {
+	vector<int> test{ 1,8,5,6,3,4,0,9,7,2 };
+
+	cout << "Pre-heapify: " << endl;
+	for (const auto& p : test)
+		cout << p << " ";
+
+	cout << "\n\n";
+
+	MinPriorityQueue<int> minPQ{ test };
+
+	cout << "Post-heapify: " << endl;
+	while (!minPQ.isEmpty())
+	{
+		cout << minPQ.min() << " ";
+		minPQ.removeMin();
+	}
+
+	cin.get();
+	cin.get();
+
 	return 0;
 }
